f2d57c5ec36fbd487969c6855b0be3e735166b77
davidyano600 <godsentryan@gmail.com>
2025-07-12 11:06:26 +0100

    myobj

--- variablesMe.ts
+++ variablesMe.ts
@@ -2,4 +2,10 @@ let greeting: string = "dave";
 let newGreeting = greeting.charAt(0).toUpperCase() + greeting.slice(1).toLowerCase()
 console.log(newGreeting);
 
+
+let hero: string;
+function getHero(){
+    return "hero"
+}
+hero = getHero()
 export{}
