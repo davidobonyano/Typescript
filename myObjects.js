a8311eb5354bc9cbc826937b1b8bba4ffdc71ee8
davidyano600 <godsentryan@gmail.com>
2025-07-13 13:21:46 +0100

    interface

---
+++ myObjects.js
@@ -0,0 +1,9 @@
+function createUser(user) {
+    return ({ name: "", email: "", isActive: false });
+}
+var Myuser = {
+    name: "dave",
+    email: "h@H.com",
+    isActive: false
+};
+Myuser.email = "h@gmail.com";
---
+++ myObjects.ts
@@ -0,0 +1,18 @@
+type user = {
+    name: string;
+    email: string;
+    isActive: boolean;
+    creditdetails?: number
+}
+
+function createUser(user: user): user{
+    return({name:"", email: "", isActive: false})
+}
+
+let
+ Myuser: user = {
+    name:"dave",
+    email: "h@H.com",
+    isActive: false
+}
+Myuser.email = "h@gmail.com"
